0cd500b0516b9bdb1798f8bd61d5330423a7a4d4
dallo <dallo@dalloui-MacBookPro.local>
2024-11-04 11:29:31 +0900

    test-commit

---
+++ src/main/java/org/example/demojvmmetric/DemoJvmMetricApplication.java
@@ -0,0 +1,13 @@
+package org.example.demojvmmetric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJvmMetricApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJvmMetricApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demojvmmetric/DemoJvmMetricApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demojvmmetric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJvmMetricApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
